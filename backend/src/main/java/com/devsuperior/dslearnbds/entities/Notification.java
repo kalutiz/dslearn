c440411276f607f6ff2890d03497c8c2c4437edb
Felipe Armando <84253307+kalutiz@users.noreply.github.com>
2022-08-15 11:17:15 -0300

    Deliver

--- backend/src/main/java/com/devsuperior/dslearnbds/entities/Notification.java
+++ backend/src/main/java/com/devsuperior/dslearnbds/entities/Notification.java
@@ -1,16 +1,8 @@
 package com.devsuperior.dslearnbds.entities;
 
+import javax.persistence.*;
 import java.io.Serializable;
 import java.time.Instant;
-
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.Table;
 
 @Entity
 @Table(name = "tb_notification")
